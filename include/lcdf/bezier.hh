9e17929fc5dbb02e283d453c18a489b1e29c3e40
Eddie Kohler <eddietwo@mit.edu>
1999-07-01 20:17:21 +0000

    added ability to output EPS

--- include/lcdf/bezier.hh
+++ include/lcdf/bezier.hh
@@ -1,127 +1,44 @@
 #ifndef BEZIER_HH
 #define BEZIER_HH
 #include "point.hh"
-#include <X11/Xlib.h>
 #include "vector.hh"
-class Charpanel;
-class Bezier;
-class Stroke;
-class Lens;
 
 class Bezier {
   
-  friend class Stroke;
-  
-  int _flags;
-  Bezier *_left;
-  Bezier *_right;
-  point _p[4];
-  
-  static XPoint *points;
-  static int npoints;
-  static int maxpoints;
-  
-  int check_bb(const point &, double);
-  double hit_recurse(double, point &, double, double, double, double);
-  
-  void segment_recurse();
-  
-  void draw_recurse(Charpanel *);
-  void unx_draw_curve(Charpanel *);
-  void unx_draw_segments(Charpanel *, int);
-  void unx_draw_controls(Charpanel *, int);
-  
-  ~Bezier();
-  
-  static const int BBValues	= 0x0FF;
+  Point _p[4];
   
  public:
   
-  static const int BBExists	= 0x100;
+  Bezier()				{ }
+  Bezier(Point p[4]);
+  Bezier(const Point &, const Point &, const Point &, const Point &);
   
-  Bezier()				: _flags(0) { }
-  Bezier(const point &, const point &, const point &, const point &);
-  Bezier(Lens *, const Bezier &);
+  const Point *points() const		{ return _p; }
+  const Point &point(int i) const	{ assert(i>=0&&i<4); return _p[i]; }
   
-  void halve(Bezier &, Bezier &) const;
+  Point eval(double) const;
   
-  int is_flat(double) const;
-  
-  int hit(point &, double);
-  
-  point eval(double) const;
-  
-  const point &p(int i) const		{ return _p[i]; }
-  point &p(int i)			{ return _p[i]; }
-  Bezier *left() const			{ return _left; }
-  Bezier *right() const			{ return _right; }
-  Bezier *side(int s) const		{ return s <= 1 ? _left : _right; }
-  Bezier *other_side(int s) const	{ return s <= 1 ? _right : _left; }
-  void insert_after(Bezier *);
-  
-  double bb_top() const;
-  double bb_left() const;
-  double bb_bottom() const;
-  double bb_right() const;
-  bool have_bb() const			{ return _flags & BBExists; }
-  void prepare_bb();
-  void ensure_bb();
-  
-  void draw_curve(Lens *, Charpanel *);
-  void draw(Lens *, Charpanel *, int);
-  
-  void segment_curve(Lens *);
-  
-  static void initialize_class(Charpanel *);
+  static void fit(const Vector<Point> &, double, Vector<Bezier> &);
   
 };
 
-#define DrawControl(i)       (1 << (i))
-#define DrawControls         (0xF)
-#define DrawSegment(i)       (1 << ((i) + 4))
-#define DrawCrossSegment     (1 << 6)
-#define DrawSegments         (0x70)
-#define DrawCurve            (1 << 7)
-#define DrawSelected         (1 << 15)
-#define DrawAll              (~DrawSelected)
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
 
 inline
-Bezier::~Bezier()
+Bezier::Bezier(Point p[4])
 {
+  _p[0] = p[0];
+  _p[1] = p[1];
+  _p[2] = p[2];
+  _p[3] = p[3];
 }
 
-inline double
-Bezier::bb_top() const
+inline
+Bezier::Bezier(const Point &p0, const Point &p1, const Point &p2, const Point &p3)
 {
-  return _p[(_flags >> 6) & 3].y;
-}
-
-inline double
-Bezier::bb_left() const
-{
-  return _p[(_flags >> 2) & 3].x;
-}
-
-inline double
-Bezier::bb_bottom() const
-{
-  return _p[(_flags >> 4) & 3].y;
-}
-
-inline double
-Bezier::bb_right() const
-{
-  return _p[(_flags >> 0) & 3].x;
-}
-
-inline void
-Bezier::ensure_bb()
-{
-  if (!have_bb())
-    prepare_bb();
+  _p[0] = p0;
+  _p[1] = p1;
+  _p[2] = p2;
+  _p[3] = p3;
 }
 
 #endif
--- liblcdf/bezier.cc
+++ liblcdf/bezier.cc
@@ -2,308 +2,227 @@
 # include <config.h>
 #endif
 #include "bezier.hh"
-#include "charpanel.hh"
-#include "segment.hh"
-#include "cntlsprt.hh"
-#include <float.h>
-#include <assert.h>
 
-Bezier::Bezier(const point &p1, const point &p2, const point &p3,
-	       const point &p4)
-  : _flags(0), _left(0), _right(0)
+Point
+eval_bezier(Point *b_in, int degree, double u)
 {
-  _p[0] = p1;
-  _p[1] = p2;
-  _p[2] = p3;
-  _p[3] = p4;
-}
-
-Bezier::Bezier(Lens *lens, const Bezier &b)
-  : _flags(0)
-{
-  _p[0] = lens->transform(b._p[0]);
-  _p[1] = lens->transform(b._p[1]);
-  _p[2] = lens->transform(b._p[2]);
-  _p[3] = lens->transform(b._p[3]);
-}
-
-
-void
-Bezier::insert_after(Bezier *b)
-{
-  assert(!_left && !b->_right);
-  _left = b;
-  b->_right = this;
-}
-
-
-int
-Bezier::is_flat(double t) const
-{
-  return _p[2].on_segment(_p[0], _p[3], t) &&
-    _p[1].on_segment(_p[0], _p[3], t);
-}
-
-
-void
-Bezier::halve(Bezier &l, Bezier &r) const
-{
-  point half = point::midpoint(_p[1], _p[2]);
-  l._p[0] = _p[0];
-  l._p[1] = point::midpoint(_p[0], _p[1]);
-  l._p[2] = point::midpoint(l._p[1], half);
-  r._p[3] = _p[3];
-  r._p[2] = point::midpoint(_p[2], _p[3]);
-  r._p[1] = point::midpoint(r._p[2], half);
-  r._p[0] = l._p[3] = point::midpoint(l._p[2], r._p[1]);
-}
-
-
-void
-Bezier::prepare_bb()
-{
-  _flags &= ~BBValues;
-  for (int i = 1; i < 4; i++) {
-    if (_p[i].x > _p[(_flags >> 0) & 3].x)
-      _flags = (_flags & ~0x03) | (i << 0);
-    else if (_p[i].x < _p[(_flags >> 2) & 3].x)
-      _flags = (_flags & ~0x0C) | (i << 2);
-    if (_p[i].y > _p[(_flags >> 4) & 3].y)
-      _flags = (_flags & ~0x30) | (i << 4);
-    else if (_p[i].y < _p[(_flags >> 6) & 3].y)
-      _flags = (_flags & ~0xC0) | (i << 6);
-  }
-  _flags |= BBExists;
-}
-
-
-int
-Bezier::check_bb(const point &hit, double tolerance)
-{
-  ensure_bb();
-  if (bb_right() + tolerance < hit.x) return 0;
-  if (bb_left() - tolerance > hit.x) return 0;
-  if (bb_bottom() + tolerance < hit.y) return 0;
-  if (bb_top() - tolerance > hit.y) return 0;
-  return 1;
-}
-
-
-double
-Bezier::hit_recurse(double tolerance, point &hit,
-		    double leftd, double rightd, double leftt, double rightt)
-{
-  Bezier left, right;
-  double middled, resultt;
+  assert(degree < 4);
+  Point b[4];
+  for (int i = 0; i <= degree; i++)
+    b[i] = b_in[i];
   
-  if (is_flat(tolerance)) {
-    if (hit.on_segment(_p[0], _p[3], tolerance)) return (leftt + rightt) / 2;
-    else return -1;
-  }
-  
-  if (leftd < tolerance * tolerance) return leftt;
-  if (rightd < tolerance * tolerance) return rightt;
-  
-  if (!check_bb(hit, tolerance)) return -1;
-  
-  halve(left, right);
-  middled = point::squared_length(right._p[0] - hit);
-  resultt = left.hit_recurse(tolerance, hit,
-			     leftd, middled, leftt, (leftt + rightt) / 2);
-  if (resultt >= 0) return resultt;
-  return right.hit_recurse(tolerance, hit,
-			   middled, rightd, (leftt + rightt) / 2, rightt);
+  double m = 1.0-u;
+  for (int i = 1; i <= degree; i++)
+    for (int j = 0; j <= degree - i; j++)
+      b[j] = b[j]*m + b[j+1]*u;
+  return b[0];
 }
 
-
-int
-Bezier::hit(point &hit, double tolerance)
+Point
+Bezier::eval(double u) const
 {
-  double leftd = point::squared_length(_p[0] - hit);
-  double rightd = point::squared_length(_p[3] - hit);
-  double resultt = hit_recurse(tolerance, hit, leftd, rightd, 0, 1);
-  return resultt >= 0;
-}
-
-
-point
-Bezier::eval(double t) const
-{
-  int i;
-  point n_p[4];
-  
-  for (i = 0; i < 4; i++)
-    n_p[i] = _p[i];
-  for (i = 1; i < 4; i++)
+  Bezier b = *this;
+  double m = 1.0-u;
+  for (int i = 1; i < 4; i++)
     for (int j = 0; j < 4 - i; j++)
-      n_p[j] = (1 - t) * n_p[j] + t * n_p[j + 1];
+      b._p[j] = m * b._p[j] + u * b._p[j+1];
+  return b._p[0];
+}
+
+
+/* Curve fitting code after Philip J. Schneider's algorithm described, and
+   code given, in the first Graphics Gems */
+
+static void
+chord_length_parameterize(const Point *d, int nd, Vector<double> &result)
+{
+  assert(result.size() == 0);
+  result.reserve(nd);
+  result.push_back(0);
+  for (int i = 1; i < nd; i++)
+    result.push_back(result.back() + Point::distance(d[i-1], d[i]));
+  double last_dist = result.back();
+  for (int i = 1; i < nd; i++)
+    result[i] /= last_dist;
+}
+
+static inline double
+B0(double u)
+{
+  double m = 1.0 - u;
+  return m*m*m;
+}
+
+static inline double
+B1(double u)
+{
+  double m = 1.0 - u;
+  return 3*m*m*u;
+}
+
+static inline double
+B2(double u)
+{
+  double m = 1.0 - u;
+  return 3*m*u*u;
+}
+
+static inline double
+B3(double u)
+{
+  return u*u*u;
+}
+
+
+static Bezier
+generate_bezier(const Point *d, int nd, const Vector<double> &parameters,
+		const Point &left_tangent, const Point &right_tangent)
+{
+  Point *a0 = new Point[nd];
+  Point *a1 = new Point[nd];
   
-  return n_p[0];
-}
-
-
-/*****
- * drawing
- **/
-
-/* bezierpoints is an array of XPoints used so we can make one call to
-   XDrawLines rather than many to XDrawLine, to reduce network traffic.
-   It is dynamically allocated so its size, maxbezierpoints, can depend on the
-   capabilities of the X server. numbezierpoints is the number of points
-   currently in the array. */
-XPoint *Bezier::points;
-int Bezier::npoints;
-int Bezier::maxpoints;
-
-void
-Bezier::segment_recurse()
-{
-  if (is_flat(0.5)) {
-    Segment::make(_p[0].x, _p[0].y, _p[3].x, _p[3].y);
-  } else {
-    Bezier left, right;
-    halve(left, right);
-    left.segment_recurse();
-    right.segment_recurse();
+  for (int i = 0; i < nd; i++) {
+    a0[i] = left_tangent * B1(parameters[i]);
+    a1[i] = right_tangent * B2(parameters[i]);
   }
+  
+  double c[2][2], x[2];
+  c[0][0] = c[0][1] = c[1][0] = c[1][1] = x[0] = x[1] = 0.0;
+  
+  int last = nd - 1;
+  for (int i = 0; i < nd; i++) {
+    c[0][0] += Point::dot(a0[i], a0[i]);
+    c[0][1] += Point::dot(a0[i], a1[i]);
+    c[1][1] += Point::dot(a1[i], a1[i]);
+    
+    Point tmp = d[i] - (d[0] * (B0(parameters[i]) + B1(parameters[i]))
+			+ d[last] * (B2(parameters[i]) + B3(parameters[i])));
+    x[0] += Point::dot(a0[i], tmp);
+    x[1] += Point::dot(a1[i], tmp);
+  }
+  c[1][0] = c[0][1];
+  
+  // compute determinants
+  double det_c0_c1 = c[0][0]*c[1][1] - c[1][0]*c[0][1];
+  double det_c0_x = c[0][0]*x[1] - c[0][1]*x[0];
+  double det_x_c1 = x[0]*c[1][1] - x[1]*c[0][1];
+
+  // finally, derive alpha values
+  if (det_c0_c1 == 0.0)
+    det_c0_c1 = c[0][0]*c[1][1] * 10e-12;
+  double alpha_l = det_x_c1 / det_c0_c1;
+  double alpha_r = det_c0_x / det_c0_c1;
+
+  // if alpha negative, use the Wu/Barsky heuristic
+  if (alpha_l < 0.0 || alpha_r < 0.0) {
+    double distance = Point::distance(d[0], d[last]) / 3;
+    return Bezier(d[0], d[0] + left_tangent*distance,
+		  d[last] + right_tangent*distance, d[last]);
+  } else
+    return Bezier(d[0], d[0] + left_tangent*alpha_l,
+		  d[last] + right_tangent*alpha_r, d[last]);
+}
+
+static double
+newton_raphson_root_find(const Bezier &b, const Point &p, double u)
+{
+  const Point *b_pts = b.points();
+  
+  Point b_det[3];
+  for (int i = 0; i < 3; i++)
+    b_det[i] = (b_pts[i+1] - b_pts[i]) * 3;
+  
+  Point b_det_det[2];
+  for (int i = 0; i < 2; i++)
+    b_det_det[i] = (b_det[i+1] - b_det[i]) * 2;
+  
+  Point b_u = b.eval(u);
+  Point b_det_u = eval_bezier(b_det, 2, u);
+  Point b_det_det_u = eval_bezier(b_det_det, 1, u);
+  
+  double numerator = Point::dot(b_u - p, b_det_u);
+  double denominator = Point::dot(b_det_u, b_det_u) +
+    Point::dot(b_u - p, b_det_det_u);
+  
+  return u - numerator/denominator;
+}
+
+static void
+reparameterize(const Point *d, int nd, Vector<double> &parameters,
+	       const Bezier &b)
+{
+  for (int i = 0; i < nd; i++)
+    parameters[i] = newton_raphson_root_find(b, d[i], parameters[i]);
 }
 
 
-void
-Bezier::segment_curve(Lens *lens)
+static double
+compute_max_error(const Point *d, int nd, const Bezier &b,
+		  const Vector<double> &parameters, int *split_point)
 {
-  Bezier winb(lens, *this);
-  winb.ensure_bb();
-  if (winb.bb_right() < 0 ||
-      winb.bb_bottom() < 0 ||
-      winb.bb_left() > lens->screen_width() ||
-      winb.bb_top() > lens->screen_height())
-    Segment::make(winb._p[0].x, winb._p[0].y, winb._p[3].x, winb._p[3].y);
-  else
-    winb.segment_recurse();
-}
-
-
-/* drawbezierrecurse  Draws the bezier b, which need not necessarily be
-     prepared (ie. its bounding box information in particular may be invalid),
-     using the recursive subdivision algorithm. b should already be in window
-     coordinates. The bezcontext c is used to specify the display (c->d),
-     window (c->w), and GC (c->gc) to draw into/with. */
-
-void
-Bezier::draw_recurse(Charpanel *c)
-{
-  if (is_flat(0.5)) {
-    if (!npoints) {
-      /* the very first point */
-      points[0].x = (int)_p[0].x;
-      points[0].y = (int)_p[0].y;
-      npoints++;
+  *split_point = nd/2;
+  double max_dist = 0.0;
+  for (int i = 1; i < nd - 1; i++) {
+    double dist = (b.eval(parameters[i]) - d[i]).squared_length();
+    if (dist >= max_dist) {
+      max_dist = dist;
+      *split_point = i;
     }
-    if (npoints == maxpoints) {
-      c->draw_outline(points, npoints);
-      points[0] = points[npoints - 1];
-      npoints = 1;
+  }
+  return max_dist;
+}
+
+
+static void
+fit0(const Point *d, int nd, Point left_tangent, Point right_tangent,
+     double error, Vector<Bezier> &result)
+{
+  // Use a heuristic for small regions (only two points)
+  if (nd == 2) {
+    double dist = Point::distance(d[0], d[1]) / 3;
+    result.push_back(Bezier(d[0],
+			    d[0] + dist*left_tangent,
+			    d[1] + dist*right_tangent,
+			    d[1]));
+    return;
+  }
+  
+  // Parameterize points and attempt to fit curve
+  Vector<double> parameters;
+  chord_length_parameterize(d, nd, parameters);
+  Bezier b = generate_bezier(d, nd, parameters, left_tangent, right_tangent);
+
+  // find max error
+  int split_point;
+  double max_error = compute_max_error(d, nd, b, parameters, &split_point);
+  if (max_error < error) {
+    result.push_back(b);
+    return;
+  }
+  
+  // if error not too large, try iteration and reparameterization
+  if (max_error < error*error)
+    for (int i = 0; i < 4; i++) {
+      reparameterize(d, nd, parameters, b);
+      b = generate_bezier(d, nd, parameters, left_tangent, right_tangent);
+      max_error = compute_max_error(d, nd, b, parameters, &split_point);
+      if (max_error < error) {
+	result.push_back(b);
+	return;
+      }
     }
-    points[npoints].x = (int)_p[3].x;
-    points[npoints].y = (int)_p[3].y;
-    npoints++;
-  } else {
-    Bezier left, right;
-    halve(left, right);
-    left.draw_recurse(c);
-    right.draw_recurse(c);
-  }
-}
-
-
-inline void
-Bezier::unx_draw_curve(Charpanel *c)
-{
-  npoints = 0;
-  draw_recurse(c);
-  if (npoints > 1)
-    c->draw_outline(points, npoints);
-}
-
-
-inline void
-Bezier::unx_draw_segments(Charpanel *c, int parts)
-{
-  if (parts & DrawSegment(0))
-    c->draw_segment((int)_p[0].x, (int)_p[0].y, (int)_p[1].x, (int)_p[1].y);
-  if (parts & DrawSegment(1))
-    c->draw_segment((int)_p[3].x, (int)_p[3].y, (int)_p[2].x, (int)_p[2].y);
-  if (parts & DrawCrossSegment)
-    c->draw_segment((int)_p[1].x, (int)_p[1].y, (int)_p[2].x, (int)_p[2].y);
-}
-
-
-inline void
-Bezier::unx_draw_controls(Charpanel *c, int parts)
-{
-  if (parts & DrawControl(0))
-    c->anchor()->draw(c, (int)_p[0].x, (int)_p[0].y);
-  if (parts & DrawControl(1))
-    c->control()->draw(c, (int)_p[1].x, (int)_p[1].y);
-  if (parts & DrawControl(2))
-    c->control()->draw(c, (int)_p[2].x, (int)_p[2].y);
-  if (parts & DrawControl(3))
-    c->anchor()->draw(c, (int)_p[3].x, (int)_p[3].y);
-}
-
-
-void
-Bezier::draw_curve(Lens *lens, Charpanel *c)
-{
-  Bezier winb(lens, *this);
-  winb.unx_draw_curve(c);
-}
-
-
-void
-Bezier::draw(Lens *lens, Charpanel *c, int parts)
-{
-  Bezier winb(lens, *this);
-  winb.unx_draw_segments(c, parts);
-  if (parts & DrawCurve)
-    winb.unx_draw_curve(c);
-  winb.unx_draw_controls(c, parts);
-}
-
-
-void
-Stroke::draw(Lens *lens, Charpanel *cp, int parts) const
-{
-  int internal_parts = parts & ~DrawControl(3);
-  Bezier::npoints = 0;
   
-  for (Bezier *b = first(); b; b = next(b)) {
-    Bezier winb(lens, *b);
-    winb.unx_draw_segments(cp, parts);
-    if (parts & DrawCurve)
-      winb.draw_recurse(cp);
-    if (b->_right)
-      winb.unx_draw_controls(cp, internal_parts);
-    else
-      winb.unx_draw_controls(cp, parts);
-  }
-  
-  if (Bezier::npoints > 1)
-    cp->draw_outline(Bezier::points, Bezier::npoints);
+  // fitting failed -- split at max error point and fit again
+  Point center_tangent = ((d[split_point-1] - d[split_point+1])/2).normal();
+  fit0(d, split_point+1, left_tangent, center_tangent, error, result);
+  fit0(d+split_point, nd-split_point, -center_tangent, right_tangent, error, result);
 }
 
-
-/* initbezierdrawing  Initializes several things for the bezier-drawing system.
-     Must be called before the first call to drawbezier(). */
-
 void
-Bezier::initialize_class(Charpanel *c) {
-  // if we've already been initialized, return immediately.
-  if (maxpoints) return;
-  
-  // create the points array
-  maxpoints = (XMaxRequestSize(c->display()) - 3) / 2;
-  if (maxpoints > 10000) maxpoints = 10000;
-  points = new XPoint[maxpoints];
+Bezier::fit(const Vector<Point> &points, double error, Vector<Bezier> &result)
+{
+  int npoints = points.size();
+  Point left_tangent = (points[1] - points[0]).normal();
+  Point right_tangent = (points[npoints-2] - points[npoints-1]).normal();
+  fit0(&points[0], npoints, left_tangent, right_tangent, error, result);
 }
