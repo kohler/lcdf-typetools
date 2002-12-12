89463a9617a97bc2313765a8eb76d0b6df4d2eb5
Eddie Kohler <ekohler@gmail.com>
2002-12-12 08:30:29 +0000

    nits

--- include/lcdf/permstr.hh
+++ include/lcdf/permstr.hh
@@ -4,56 +4,54 @@
 #include <cstddef>
 #include <cstdarg>
 
-class PermString { struct Doodad;
+class PermString { struct Doodad; public:
+  
+    typedef Doodad *Capsule;
+    // Declare a PermString::Initializer in any file in which you declare
+    // static global PermStrings.
+    struct Initializer { Initializer(); };
+  
+    PermString()				: _rep(0) { }
+    PermString(int i)			: _rep(0) { assert(!i); }
+    explicit PermString(char c);
+    PermString(const char *);
+    PermString(const char *, int);
+  
+    operator bool() const			{ return _rep != 0; }
+    int length() const			{ return doodad()->length; }
+  
+    friend bool operator==(PermString, PermString);
+    friend bool operator!=(PermString, PermString);
+  
+    const char *cc() const		{ return _rep; }
+    operator const char *() const		{ return _rep; }
+  
+    Capsule capsule() const		{ return doodad(); }
+    static PermString decapsule(Capsule c) { return PermString(c); }
+  
+    friend PermString permprintf(const char *, ...);
+    friend PermString vpermprintf(const char *, va_list);
+    friend PermString permcat(PermString, PermString);
+    friend PermString permcat(PermString, PermString, PermString);  
 
- public:
+  private:
   
-  typedef Doodad *Capsule;
-  // Declare a PermString::Initializer in any file in which you declare
-  // static global PermStrings.
-  struct Initializer { Initializer(); };
+    struct Doodad {
+	Doodad *next;
+	int length;
+	char data[2];
+    };
   
-  PermString()				: _rep(0) { }
-  PermString(int i)			: _rep(0) { assert(!i); }
-  explicit PermString(char c);
-  PermString(const char *);
-  PermString(const char *, int);
+    char *_rep;
   
-  operator bool() const			{ return _rep != 0; }
-  int length() const			{ return doodad()->length; }
+    PermString(Doodad *d)			: _rep(d->data) { }
+    Doodad *doodad() const { return (Doodad *)(_rep - offsetof(Doodad, data)); }
   
-  friend bool operator==(PermString, PermString);
-  friend bool operator!=(PermString, PermString);
-  
-  const char *cc() const		{ return _rep; }
-  operator const char *() const		{ return _rep; }
-  
-  Capsule capsule() const		{ return doodad(); }
-  static PermString decapsule(Capsule c) { return PermString(c); }
-  
-  friend PermString permprintf(const char *, ...);
-  friend PermString vpermprintf(const char *, va_list);
-  friend PermString permcat(PermString, PermString);
-  friend PermString permcat(PermString, PermString, PermString);  
+    friend class PermString::Initializer;
+    static void static_initialize();
 
- private:
-  
-  struct Doodad {
-    Doodad *next;
-    int length;
-    char data[2];
-  };
-  
-  char *_rep;
-  
-  PermString(Doodad *d)			: _rep(d->data) { }
-  Doodad *doodad() const { return (Doodad *)(_rep - offsetof(Doodad, data)); }
-  
-  friend class PermString::Initializer;
-  static void static_initialize();
-
-  static const int NHASH = 1024; // must be power of 2
-  static Doodad zero_char_doodad, one_char_doodad[256], *buckets[NHASH];
+    static const int NHASH = 1024; // must be power of 2
+    static Doodad zero_char_doodad, one_char_doodad[256], *buckets[NHASH];
   
 };
 
@@ -61,7 +59,7 @@ class PermString { struct Doodad;
 inline bool
 operator==(PermString a, PermString b)
 {
-  return a._rep == b._rep;
+    return a._rep == b._rep;
 }
 
 bool operator==(PermString, const char *);
@@ -69,25 +67,25 @@ bool operator==(PermString, const char *);
 inline bool
 operator==(const char *a, PermString b)
 {
-  return b == a;
+    return b == a;
 }
 
 inline bool
 operator!=(PermString a, PermString b)
 {
-  return a._rep != b._rep;
+    return a._rep != b._rep;
 }
 
 inline bool
 operator!=(PermString a, const char *b)
 {
-  return !(a == b);
+    return !(a == b);
 }
 
 inline bool
 operator!=(const char *a, PermString b)
 {
-  return !(b == a);
+    return !(b == a);
 }
 
 inline int
