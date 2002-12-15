09ccb12b45f3e2f6a5339ae038870f115ace5e99
Eddie Kohler <ekohler@gmail.com>
2002-12-15 22:19:17 +0000

    remove // comments, clarify logic

--- liblcdf/strtonum.c
+++ liblcdf/strtonum.c
@@ -16,16 +16,9 @@ extern "C" {
 double
 strtonumber(const char *f, char **endf)
 {
-  long v;
-  
-  // in case strtol() doesn't return 0 on strange input
-  if (*f == '.')
-    return strtod((char *)f, endf);
+  long v = strtol((char *)f, endf, 10);
 
-  // get integer part
-  v = strtol((char *)f, endf, 10);
-
-  // handle any possible decimal part
+  /* handle any possible decimal part */
   if (**endf == '.' || **endf == 'E' || **endf == 'e')
     return strtod((char *)f, endf);
   else
