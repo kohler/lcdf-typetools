e2cf9514160b3fa46bcfed234d4ea1b1c78f64ab
Eddie Kohler <ekohler@gmail.com>
2002-12-30 23:54:58 +0000

    Bezier cleanups

--- include/lcdf/bezier.hh
+++ include/lcdf/bezier.hh
@@ -3,116 +3,159 @@
 #include <lcdf/transform.hh>
 #include <lcdf/vector.hh>
 
-class Bezier {
+class Bezier { public:
   
-  Point _p[4];
-  mutable int _bb;
+    Bezier()				: _bb(-1) { }
+    Bezier(Point p[4]);
+    Bezier(const Point &, const Point &, const Point &, const Point &);
+    Bezier(const Bezier &, const Transform &);
+  
+    const Point *points() const		{ return _p; }
+    const Point &point(int i) const	{ assert(i>=0&&i<4); return _p[i]; }
+    void set_point(int i, const Point &p) { assert(i>=0&&i<4);_p[i]=p;_bb=-1; }
 
-  void make_bb() const;
-  void ensure_bb() const;
-  
-  double hit_recurse(const Point &, double, double, double, double, double) const;
+    Point eval(double) const;
+    bool is_flat(double) const;
+    bool in_bb(const Point &, double) const;
+    bool hit(const Point &, double) const;
 
- public:
-  
-  Bezier()				{ }
-  Bezier(Point p[4]);
-  Bezier(const Point &, const Point &, const Point &, const Point &);
-  
-  const Point *points() const		{ return _p; }
-  Point &point(int i)			{ assert(i>=0&&i<4); return _p[i]; }
-  const Point &point(int i) const	{ assert(i>=0&&i<4); return _p[i]; }
-  
-  Point eval(double) const;
-  bool is_flat(double) const;
-  bool in_bb(const Point &, double) const;
-  bool hit(const Point &, double) const;
+    double bb_left() const;
+    double bb_right() const;
+    double bb_top() const;
+    double bb_bottom() const;
 
-  double bb_left() const;
-  double bb_right() const;
-  double bb_top() const;
-  double bb_bottom() const;
+    double bb_left_x() const;
+    double bb_right_x() const;
+    double bb_top_x() const;
+    double bb_bottom_x() const;
+    
+    void halve(Bezier &, Bezier &) const;
 
-  void halve(Bezier &, Bezier &) const;
+    void segmentize(Vector<Point> &) const;
+    void segmentize(Vector<Point> &, bool) const;
+    void segmentize(Vector<Point> &, const Transform &) const;
+  
+    static void fit(const Vector<Point> &, double, Vector<Bezier> &);
+    
+  private:
+  
+    Point _p[4];
+    mutable int _bb;
 
-  void segmentize(Vector<Point> &) const;
-  void segmentize(Vector<Point> &, bool) const;
-  void segmentize(Vector<Point> &, const Transform &) const;
+    void make_bb() const;
+    void ensure_bb() const;
   
-  static void fit(const Vector<Point> &, double, Vector<Bezier> &);
-  
+    double hit_recurse(const Point &, double, double, double, double, double) const;
+
 };
 
 
 inline
 Bezier::Bezier(Point p[4])
 {
-  _p[0] = p[0];
-  _p[1] = p[1];
-  _p[2] = p[2];
-  _p[3] = p[3];
-  _bb = -1;
+    _p[0] = p[0];
+    _p[1] = p[1];
+    _p[2] = p[2];
+    _p[3] = p[3];
+    _bb = -1;
 }
 
 inline
 Bezier::Bezier(const Point &p0, const Point &p1, const Point &p2, const Point &p3)
 {
-  _p[0] = p0;
-  _p[1] = p1;
-  _p[2] = p2;
-  _p[3] = p3;
-  _bb = -1;
+    _p[0] = p0;
+    _p[1] = p1;
+    _p[2] = p2;
+    _p[3] = p3;
+    _bb = -1;
 }
 
-inline double
-Bezier::bb_top() const
+inline
+Bezier::Bezier(const Bezier &b, const Transform &t)
 {
-  return _p[(_bb >> 4) & 3].y;
-}
-
-inline double
-Bezier::bb_left() const
-{
-  return _p[(_bb >> 2) & 3].x;
-}
-
-inline double
-Bezier::bb_bottom() const
-{
-  return _p[(_bb >> 6) & 3].y;
-}
-
-inline double
-Bezier::bb_right() const
-{
-  return _p[(_bb >> 0) & 3].x;
+    _p[0] = b._p[0] * t;
+    _p[1] = b._p[1] * t;
+    _p[2] = b._p[2] * t;
+    _p[3] = b._p[3] * t;
+    _bb = -1;
 }
 
 inline void
 Bezier::ensure_bb() const
 {
-  if (_bb < 0)
-    make_bb();
+    if (_bb < 0)
+	make_bb();
+}
+
+inline double
+Bezier::bb_top_x() const
+{
+    return _p[(_bb >> 4) & 3].y;
+}
+
+inline double
+Bezier::bb_left_x() const
+{
+    return _p[(_bb >> 2) & 3].x;
+}
+
+inline double
+Bezier::bb_bottom_x() const
+{
+    return _p[(_bb >> 6) & 3].y;
+}
+
+inline double
+Bezier::bb_right_x() const
+{
+    return _p[(_bb >> 0) & 3].x;
+}
+
+inline double
+Bezier::bb_top() const
+{
+    ensure_bb();
+    return bb_top_x();
+}
+
+inline double
+Bezier::bb_left() const
+{
+    ensure_bb();
+    return bb_left_x();
+}
+
+inline double
+Bezier::bb_bottom() const
+{
+    ensure_bb();
+    return bb_bottom_x();
+}
+
+inline double
+Bezier::bb_right() const
+{
+    ensure_bb();
+    return bb_right_x();
 }
 
 inline void
 Bezier::segmentize(Vector<Point> &v) const
 {
-  segmentize(v, v.size() == 0 || v.back() != _p[0]);
+    segmentize(v, v.size() == 0 || v.back() != _p[0]);
 }
 
 inline void
 Bezier::segmentize(Vector<Point> &v, const Transform &t) const
 {
-  Bezier b(_p[0] * t, _p[1] * t, _p[2] * t, _p[3] * t);
-  b.segmentize(v, v.size() == 0 || v.back() != b._p[0]);
+    Bezier b(*this, t);
+    b.segmentize(v, v.size() == 0 || v.back() != b._p[0]);
 }
 
 inline Bezier
 operator*(const Bezier &b, const Transform &t)
 {
-  return Bezier(b.point(0) * t, b.point(1) * t,
-		b.point(2) * t, b.point(3) * t);
+    return Bezier(b, t);
 }
 
 #endif
--- liblcdf/bezier.cc
+++ liblcdf/bezier.cc
@@ -10,17 +10,17 @@
 void
 Bezier::make_bb() const
 {
-  _bb = 0;
-  for (int i = 1; i < 4; i++) {
-    if (_p[i].x > bb_right())
-      _bb = (_bb & ~0x03) | (i << 0);
-    else if (_p[i].x < bb_left())
-      _bb = (_bb & ~0x0C) | (i << 2);
-    if (_p[i].y > bb_top())
-      _bb = (_bb & ~0x30) | (i << 4);
-    else if (_p[i].y < bb_bottom())
-      _bb = (_bb & ~0xC0) | (i << 6);
-  }
+    _bb = 0;
+    for (int i = 1; i < 4; i++) {
+	if (_p[i].x > bb_right_x())
+	    _bb = (_bb & ~0x03) | (i << 0);
+	else if (_p[i].x < bb_left_x())
+	    _bb = (_bb & ~0x0C) | (i << 2);
+	if (_p[i].y > bb_top_x())
+	    _bb = (_bb & ~0x30) | (i << 4);
+	else if (_p[i].y < bb_bottom_x())
+	    _bb = (_bb & ~0xC0) | (i << 6);
+    }
 }
 
 
@@ -31,34 +31,34 @@ Bezier::make_bb() const
 bool
 Bezier::is_flat(double t) const
 {
-  return _p[2].on_segment(_p[0], _p[3], t) &&
-    _p[1].on_segment(_p[0], _p[3], t);
+    return (_p[2].on_segment(_p[0], _p[3], t)
+	    && _p[1].on_segment(_p[0], _p[3], t));
 }
 
 static Point
 eval_bezier(Point *b_in, int degree, double u)
 {
-  assert(degree < 4);
-  Point b[4];
-  for (int i = 0; i <= degree; i++)
-    b[i] = b_in[i];
-  
-  double m = 1.0-u;
-  for (int i = 1; i <= degree; i++)
-    for (int j = 0; j <= degree - i; j++)
-      b[j] = b[j]*m + b[j+1]*u;
-  return b[0];
+    assert(degree < 4);
+    Point b[4];
+    for (int i = 0; i <= degree; i++)
+	b[i] = b_in[i];
+
+    double m = 1.0 - u;
+    for (int i = 1; i <= degree; i++)
+	for (int j = 0; j <= degree - i; j++)
+	    b[j] = b[j]*m + b[j+1]*u;
+    return b[0];
 }
 
 Point
 Bezier::eval(double u) const
 {
-  Bezier b = *this;
-  double m = 1.0-u;
-  for (int i = 1; i < 4; i++)
-    for (int j = 0; j < 4 - i; j++)
-      b._p[j] = m * b._p[j] + u * b._p[j+1];
-  return b._p[0];
+    Bezier b = *this;
+    double m = 1.0 - u;
+    for (int i = 1; i < 4; i++)
+	for (int j = 0; j < 4 - i; j++)
+	    b._p[j] = m * b._p[j] + u * b._p[j+1];
+    return b._p[0];
 }
 
 
@@ -69,14 +69,14 @@ Bezier::eval(double u) const
 void
 Bezier::halve(Bezier &l, Bezier &r) const
 {
-  Point half = Point::midpoint(_p[1], _p[2]);
-  l._p[0] = _p[0];
-  l._p[1] = Point::midpoint(_p[0], _p[1]);
-  l._p[2] = Point::midpoint(l._p[1], half);
-  r._p[3] = _p[3];
-  r._p[2] = Point::midpoint(_p[2], _p[3]);
-  r._p[1] = Point::midpoint(r._p[2], half);
-  r._p[0] = l._p[3] = Point::midpoint(l._p[2], r._p[1]);
+    Point half = Point::midpoint(_p[1], _p[2]);
+    l._p[0] = _p[0];
+    l._p[1] = Point::midpoint(_p[0], _p[1]);
+    l._p[2] = Point::midpoint(l._p[1], half);
+    r._p[3] = _p[3];
+    r._p[2] = Point::midpoint(_p[2], _p[3]);
+    r._p[1] = Point::midpoint(r._p[2], half);
+    r._p[0] = l._p[3] = Point::midpoint(l._p[2], r._p[1]);
 }
 
 
@@ -87,56 +87,56 @@ Bezier::halve(Bezier &l, Bezier &r) const
 bool
 Bezier::in_bb(const Point &p, double tolerance) const
 {
-  ensure_bb();
-  if (bb_right() + tolerance < p.x
-      || bb_left() - tolerance > p.x
-      || bb_top() + tolerance < p.y
-      || bb_bottom() - tolerance > p.y)
-    return false;
-  else
-    return true;
+    ensure_bb();
+    if (bb_right() + tolerance < p.x
+	|| bb_left() - tolerance > p.x
+	|| bb_top() + tolerance < p.y
+	|| bb_bottom() - tolerance > p.y)
+	return false;
+    else
+	return true;
 }
 
 double
 Bezier::hit_recurse(const Point &p, double tolerance, double leftd,
 		    double rightd, double leftt, double rightt) const
 {
-  Bezier left, right;
-  double middled, resultt;
-  
-  if (is_flat(tolerance)) {
-    if (p.on_segment(_p[0], _p[3], tolerance))
-      return (leftt + rightt) / 2;
-    else
-      return -1;
-  }
-  
-  if (leftd < tolerance * tolerance)
-    return leftt;
-  if (rightd < tolerance * tolerance)
-    return rightt;
-  
-  if (!in_bb(p, tolerance))
-    return -1;
-  
-  halve(left, right);
-  middled = (right._p[0] - p).squared_length();
-  resultt = left.hit_recurse
-    (p, tolerance, leftd, middled, leftt, (leftt + rightt) / 2);
-  if (resultt >= 0)
-    return resultt;
-  
-  return right.hit_recurse
-    (p, tolerance, middled, rightd, (leftt + rightt) / 2, rightt);
+    Bezier left, right;
+    double middled, resultt;
+
+    if (is_flat(tolerance)) {
+	if (p.on_segment(_p[0], _p[3], tolerance))
+	    return (leftt + rightt) / 2;
+	else
+	    return -1;
+    }
+
+    if (leftd < tolerance * tolerance)
+	return leftt;
+    if (rightd < tolerance * tolerance)
+	return rightt;
+
+    if (!in_bb(p, tolerance))
+	return -1;
+
+    halve(left, right);
+    middled = (right._p[0] - p).squared_length();
+    resultt = left.hit_recurse
+	(p, tolerance, leftd, middled, leftt, (leftt + rightt) / 2);
+    if (resultt >= 0)
+	return resultt;
+
+    return right.hit_recurse
+	(p, tolerance, middled, rightd, (leftt + rightt) / 2, rightt);
 }
 
 bool
 Bezier::hit(const Point &p, double tolerance) const
 {
-  double leftd = (_p[0] - p).squared_length();
-  double rightd = (_p[3] - p).squared_length();
-  double resultt = hit_recurse(p, tolerance, leftd, rightd, 0, 1);
-  return resultt >= 0;
+    double leftd = (_p[0] - p).squared_length();
+    double rightd = (_p[3] - p).squared_length();
+    double resultt = hit_recurse(p, tolerance, leftd, rightd, 0, 1);
+    return resultt >= 0;
 }
 
 
@@ -149,16 +149,16 @@ Bezier::hit(const Point &p, double tolerance) const
 void
 Bezier::segmentize(Vector<Point> &v, bool first) const
 {
-  if (is_flat(0.5)) {
-    if (first)
-      v.push_back(_p[0]);
-    v.push_back(_p[3]);
-  } else {
-    Bezier left, right;
-    halve(left, right);
-    left.segmentize(v, first);
-    right.segmentize(v, false);
-  }
+    if (is_flat(0.5)) {
+	if (first)
+	    v.push_back(_p[0]);
+	v.push_back(_p[3]);
+    } else {
+	Bezier left, right;
+	halve(left, right);
+	left.segmentize(v, first);
+	right.segmentize(v, false);
+    }
 }
 
 
@@ -172,90 +172,90 @@ Bezier::segmentize(Vector<Point> &v, bool first) const
 static void
 chord_length_parameterize(const Point *d, int nd, Vector<double> &result)
 {
-  assert(result.size() == 0);
-  result.reserve(nd);
-  result.push_back(0);
-  for (int i = 1; i < nd; i++)
-    result.push_back(result.back() + Point::distance(d[i-1], d[i]));
-  double last_dist = result.back();
-  for (int i = 1; i < nd; i++)
-    result[i] /= last_dist;
+    assert(result.size() == 0);
+    result.reserve(nd);
+    result.push_back(0);
+    for (int i = 1; i < nd; i++)
+	result.push_back(result.back() + Point::distance(d[i-1], d[i]));
+    double last_dist = result.back();
+    for (int i = 1; i < nd; i++)
+	result[i] /= last_dist;
 }
 
 static inline double
 B0(double u)
 {
-  double m = 1.0 - u;
-  return m*m*m;
+    double m = 1.0 - u;
+    return m*m*m;
 }
 
 static inline double
 B1(double u)
 {
-  double m = 1.0 - u;
-  return 3*m*m*u;
+    double m = 1.0 - u;
+    return 3*m*m*u;
 }
 
 static inline double
 B2(double u)
 {
-  double m = 1.0 - u;
-  return 3*m*u*u;
+    double m = 1.0 - u;
+    return 3*m*u*u;
 }
 
 static inline double
 B3(double u)
 {
-  return u*u*u;
+    return u*u*u;
 }
 
 static Bezier
 generate_bezier(const Point *d, int nd, const Vector<double> &parameters,
 		const Point &left_tangent, const Point &right_tangent)
 {
-  Point *a0 = new Point[nd];
-  Point *a1 = new Point[nd];
-  
-  for (int i = 0; i < nd; i++) {
-    a0[i] = left_tangent * B1(parameters[i]);
-    a1[i] = right_tangent * B2(parameters[i]);
-  }
-  
-  double c[2][2], x[2];
-  c[0][0] = c[0][1] = c[1][0] = c[1][1] = x[0] = x[1] = 0.0;
-  
-  int last = nd - 1;
-  for (int i = 0; i < nd; i++) {
-    c[0][0] += Point::dot(a0[i], a0[i]);
-    c[0][1] += Point::dot(a0[i], a1[i]);
-    c[1][1] += Point::dot(a1[i], a1[i]);
+    Point *a0 = new Point[nd];
+    Point *a1 = new Point[nd];
+
+    for (int i = 0; i < nd; i++) {
+	a0[i] = left_tangent * B1(parameters[i]);
+	a1[i] = right_tangent * B2(parameters[i]);
+    }
     
-    Point tmp = d[i] - (d[0] * (B0(parameters[i]) + B1(parameters[i]))
-			+ d[last] * (B2(parameters[i]) + B3(parameters[i])));
-    x[0] += Point::dot(a0[i], tmp);
-    x[1] += Point::dot(a1[i], tmp);
-  }
-  c[1][0] = c[0][1];
+    double c[2][2], x[2];
+    c[0][0] = c[0][1] = c[1][0] = c[1][1] = x[0] = x[1] = 0.0;
   
-  // compute determinants
-  double det_c0_c1 = c[0][0]*c[1][1] - c[1][0]*c[0][1];
-  double det_c0_x = c[0][0]*x[1] - c[0][1]*x[0];
-  double det_x_c1 = x[0]*c[1][1] - x[1]*c[0][1];
+    int last = nd - 1;
+    for (int i = 0; i < nd; i++) {
+	c[0][0] += Point::dot(a0[i], a0[i]);
+	c[0][1] += Point::dot(a0[i], a1[i]);
+	c[1][1] += Point::dot(a1[i], a1[i]);
+    
+	Point tmp = d[i] - (d[0] * (B0(parameters[i]) + B1(parameters[i]))
+			    + d[last] * (B2(parameters[i]) + B3(parameters[i])));
+	x[0] += Point::dot(a0[i], tmp);
+	x[1] += Point::dot(a1[i], tmp);
+    }
+    c[1][0] = c[0][1];
+    
+    // compute determinants
+    double det_c0_c1 = c[0][0]*c[1][1] - c[1][0]*c[0][1];
+    double det_c0_x = c[0][0]*x[1] - c[0][1]*x[0];
+    double det_x_c1 = x[0]*c[1][1] - x[1]*c[0][1];
 
-  // finally, derive alpha values
-  if (det_c0_c1 == 0.0)
-    det_c0_c1 = c[0][0]*c[1][1] * 10e-12;
-  double alpha_l = det_x_c1 / det_c0_c1;
-  double alpha_r = det_c0_x / det_c0_c1;
+    // finally, derive alpha values
+    if (det_c0_c1 == 0.0)
+	det_c0_c1 = c[0][0]*c[1][1] * 10e-12;
+    double alpha_l = det_x_c1 / det_c0_c1;
+    double alpha_r = det_c0_x / det_c0_c1;
 
-  // if alpha negative, use the Wu/Barsky heuristic
-  if (alpha_l < 0.0 || alpha_r < 0.0) {
-    double distance = Point::distance(d[0], d[last]) / 3;
-    return Bezier(d[0], d[0] + left_tangent*distance,
-		  d[last] + right_tangent*distance, d[last]);
-  } else
-    return Bezier(d[0], d[0] + left_tangent*alpha_l,
-		  d[last] + right_tangent*alpha_r, d[last]);
+    // if alpha negative, use the Wu/Barsky heuristic
+    if (alpha_l < 0.0 || alpha_r < 0.0) {
+	double distance = Point::distance(d[0], d[last]) / 3;
+	return Bezier(d[0], d[0] + left_tangent*distance,
+		      d[last] + right_tangent*distance, d[last]);
+    } else
+	return Bezier(d[0], d[0] + left_tangent*alpha_l,
+		      d[last] + right_tangent*alpha_r, d[last]);
 }
 
 static double
@@ -286,76 +286,76 @@ static void
 reparameterize(const Point *d, int nd, Vector<double> &parameters,
 	       const Bezier &b)
 {
-  for (int i = 0; i < nd; i++)
-    parameters[i] = newton_raphson_root_find(b, d[i], parameters[i]);
+    for (int i = 0; i < nd; i++)
+	parameters[i] = newton_raphson_root_find(b, d[i], parameters[i]);
 }
 
 static double
 compute_max_error(const Point *d, int nd, const Bezier &b,
 		  const Vector<double> &parameters, int *split_point)
 {
-  *split_point = nd/2;
-  double max_dist = 0.0;
-  for (int i = 1; i < nd - 1; i++) {
-    double dist = (b.eval(parameters[i]) - d[i]).squared_length();
-    if (dist >= max_dist) {
-      max_dist = dist;
-      *split_point = i;
+    *split_point = nd/2;
+    double max_dist = 0.0;
+    for (int i = 1; i < nd - 1; i++) {
+	double dist = (b.eval(parameters[i]) - d[i]).squared_length();
+	if (dist >= max_dist) {
+	    max_dist = dist;
+	    *split_point = i;
+	}
     }
-  }
-  return max_dist;
+    return max_dist;
 }
 
 static void
 fit0(const Point *d, int nd, Point left_tangent, Point right_tangent,
      double error, Vector<Bezier> &result)
 {
-  // Use a heuristic for small regions (only two points)
-  if (nd == 2) {
-    double dist = Point::distance(d[0], d[1]) / 3;
-    result.push_back(Bezier(d[0],
-			    d[0] + dist*left_tangent,
-			    d[1] + dist*right_tangent,
-			    d[1]));
-    return;
-  }
-  
-  // Parameterize points and attempt to fit curve
-  Vector<double> parameters;
-  chord_length_parameterize(d, nd, parameters);
-  Bezier b = generate_bezier(d, nd, parameters, left_tangent, right_tangent);
-
-  // find max error
-  int split_point;
-  double max_error = compute_max_error(d, nd, b, parameters, &split_point);
-  if (max_error < error) {
-    result.push_back(b);
-    return;
-  }
-  
-  // if error not too large, try iteration and reparameterization
-  if (max_error < error*error)
-    for (int i = 0; i < 4; i++) {
-      reparameterize(d, nd, parameters, b);
-      b = generate_bezier(d, nd, parameters, left_tangent, right_tangent);
-      max_error = compute_max_error(d, nd, b, parameters, &split_point);
-      if (max_error < error) {
+    // Use a heuristic for small regions (only two points)
+    if (nd == 2) {
+	double dist = Point::distance(d[0], d[1]) / 3;
+	result.push_back(Bezier(d[0],
+				d[0] + dist*left_tangent,
+				d[1] + dist*right_tangent,
+				d[1]));
+	return;
+    }
+    
+    // Parameterize points and attempt to fit curve
+    Vector<double> parameters;
+    chord_length_parameterize(d, nd, parameters);
+    Bezier b = generate_bezier(d, nd, parameters, left_tangent, right_tangent);
+    
+    // find max error
+    int split_point;
+    double max_error = compute_max_error(d, nd, b, parameters, &split_point);
+    if (max_error < error) {
 	result.push_back(b);
 	return;
-      }
     }
   
-  // fitting failed -- split at max error point and fit again
-  Point center_tangent = ((d[split_point-1] - d[split_point+1])/2).normal();
-  fit0(d, split_point+1, left_tangent, center_tangent, error, result);
-  fit0(d+split_point, nd-split_point, -center_tangent, right_tangent, error, result);
+    // if error not too large, try iteration and reparameterization
+    if (max_error < error*error)
+	for (int i = 0; i < 4; i++) {
+	    reparameterize(d, nd, parameters, b);
+	    b = generate_bezier(d, nd, parameters, left_tangent, right_tangent);
+	    max_error = compute_max_error(d, nd, b, parameters, &split_point);
+	    if (max_error < error) {
+		result.push_back(b);
+		return;
+	    }
+	}
+    
+    // fitting failed -- split at max error point and fit again
+    Point center_tangent = ((d[split_point-1] - d[split_point+1])/2).normal();
+    fit0(d, split_point+1, left_tangent, center_tangent, error, result);
+    fit0(d+split_point, nd-split_point, -center_tangent, right_tangent, error, result);
 }
 
 void
 Bezier::fit(const Vector<Point> &points, double error, Vector<Bezier> &result)
 {
-  int npoints = points.size();
-  Point left_tangent = (points[1] - points[0]).normal();
-  Point right_tangent = (points[npoints-2] - points[npoints-1]).normal();
-  fit0(&points[0], npoints, left_tangent, right_tangent, error, result);
+    int npoints = points.size();
+    Point left_tangent = (points[1] - points[0]).normal();
+    Point right_tangent = (points[npoints-2] - points[npoints-1]).normal();
+    fit0(&points[0], npoints, left_tangent, right_tangent, error, result);
 }
