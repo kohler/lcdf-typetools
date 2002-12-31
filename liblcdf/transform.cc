ae1debb982947bc8d5579ea870e315f05af8d098
Eddie Kohler <ekohler@gmail.com>
2002-12-31 18:44:47 +0000

    add is_default()

--- liblcdf/transform.cc
+++ liblcdf/transform.cc
@@ -8,6 +8,7 @@ Transform::Transform()
 {
     _m[0] = _m[4] = 1;
     _m[1] = _m[2] = _m[3] = _m[5] = 0;
+    _default = true;
 }
 
 Transform::Transform(double m0, double m1, double m2,
@@ -19,6 +20,7 @@ Transform::Transform(double m0, double m1, double m2,
     _m[3] = m3;
     _m[4] = m4;
     _m[5] = m5;
+    _default = (m2 == 0 && m0 == 1 && m1 == 0 && m3 == 0 && m4 == 1 && m5 == 0);
 }
 
 
@@ -29,6 +31,9 @@ Transform::add_scale(double x, double y)
     _m[1] *= y;
     _m[3] *= x;
     _m[4] *= y;
+
+    if (x != 1 || y != 1)
+	_default = false;
 }
 
 void
@@ -44,6 +49,9 @@ Transform::add_rotate(double r)
     a = _m[3], b = _m[4];
     _m[3] = a*c + b*s;
     _m[4] = b*c - a*s;
+
+    if (r != 0)
+	_default = false;
 }
 
 void
@@ -51,6 +59,9 @@ Transform::add_translate(double x, double y)
 {
     _m[2] += _m[0]*x + _m[1]*y;
     _m[5] += _m[3]*x + _m[4]*y;
+
+    if (x != 0 || y != 0)
+	_default = false;
 }
 
 Transform
@@ -68,31 +79,39 @@ Transform::transform(const Transform &t) const
 Point &
 operator*=(Point &p, const Transform &t)
 {
-    double x = p.x;
-    p.x = x*t._m[0] + p.y*t._m[1] + t._m[2];
-    p.y = x*t._m[3] + p.y*t._m[4] + t._m[5];
+    if (!t.is_default()) {
+	double x = p.x;
+	p.x = x*t._m[0] + p.y*t._m[1] + t._m[2];
+	p.y = x*t._m[3] + p.y*t._m[4] + t._m[5];
+    }
     return p;
 }
 
 Point
 operator*(const Point &p, const Transform &t)
 {
-    return Point(p.x*t._m[0] + p.y*t._m[1] + t._m[2],
-		 p.x*t._m[3] + p.y*t._m[4] + t._m[5]);
+    return (t.is_default()
+	    ? p
+	    : Point(p.x*t._m[0] + p.y*t._m[1] + t._m[2],
+		    p.x*t._m[3] + p.y*t._m[4] + t._m[5]));
 }
 
 Bezier &
 operator*=(Bezier &b, const Transform &t)
 {
-    b.mpoint(0) *= t;
-    b.mpoint(1) *= t;
-    b.mpoint(2) *= t;
-    b.mpoint(3) *= t;
+    if (!t.is_default()) {
+	b.mpoint(0) *= t;
+	b.mpoint(1) *= t;
+	b.mpoint(2) *= t;
+	b.mpoint(3) *= t;
+    }
     return b;
 }
 
 Bezier
 operator*(const Bezier &b, const Transform &t)
 {
-    return Bezier(b.point(0) * t, b.point(1) * t, b.point(2) * t, b.point(3) * t);
+    return (t.is_default()
+	    ? b
+	    : Bezier(b.point(0) * t, b.point(1) * t, b.point(2) * t, b.point(3) * t));
 }
