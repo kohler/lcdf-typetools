ebb2287dc46640c9b465055abcbebe9dcaa9f78e
Eddie Kohler <ekohler@gmail.com>
2002-12-31 21:48:48 +0000

    updates, and add otftoafm

---
+++ otftotfm/templatei.cc
@@ -0,0 +1,24 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <lcdf/hashmap.cc>
+#include <lcdf/vector.cc>
+
+#include <lcdf/permstr.hh>
+#include <lcdf/string.hh>
+#include <lcdf/bezier.hh>
+
+#include <efont/t1item.hh>
+
+template class Vector<int>;
+template class Vector<unsigned>;
+template class Vector<double>;
+template class Vector< Vector<double> >;
+
+template class Vector<PermString>;
+template class Vector<String>;
+template class Vector<Point>;
+template class Vector<Bezier>;
+
+template class HashMap<PermString, Efont::Type1Definition *>;
+template class HashMap<PermString, int>;
