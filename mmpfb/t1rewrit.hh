d7e8a1b5455946de4de046f91f39a4a411cd9b95
Eddie Kohler <ekohler@gmail.com>
2003-07-24 19:17:04 +0000

    use the global Type1CharstringGen

--- mmpfb/t1rewrit.hh
+++ mmpfb/t1rewrit.hh
@@ -4,35 +4,6 @@
 #include <efont/t1font.hh>
 #include <lcdf/straccum.hh>
 
-class Type1CharstringGen { public:
-
-    Type1CharstringGen(int precision = 5);
-
-    void clear();
-    char *data() const			{ return _ncs.data(); }
-    int length() const			{ return _ncs.length(); }
-
-    void gen_number(double, int = 0);
-    void gen_command(int);
-    void gen_stack(Efont::CharstringInterp &, int for_cmd);
-
-    Efont::Type1Charstring *output();
-    void output(Efont::Type1Charstring &);
-
-  private:
-  
-    StringAccum _ncs;
-    int _precision;
-    double _f_precision;
-    
-    double _true_x;
-    double _true_y;
-    double _false_x;
-    double _false_y;
-  
-};
-
-
 class Type1MMRemover {
   
   Efont::Type1Font *_font;
