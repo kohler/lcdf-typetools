3e0a8555d2cca3fed32dfc85fc5c84c6d975355b
Eddie Kohler <ekohler@gmail.com>
2010-09-06 11:49:23 -0700

    Add make-glyphtounicode.pl script.
    This script generates glyphtounicode.tex, a file useful for pdflatex, from
    LCDF Typetools' glyphlist files.  We also add an override file,
    texglyphlist-g2u.txt, used only in that process.

---
+++ make-glyphtounicode.pl
@@ -0,0 +1,36 @@
+#! /usr/bin/perl
+my(%gmap, $maxversion, %versions);
+$maxversion = 0;
+
+sub doread ($) {
+    my($fn) = @_;
+    open(O, $fn) or die;
+    while (<O>) {
+	if (/^\#.*Version ([0-9.]+)/ || /^\# Table version:\s*([0-9.]+)/) {
+	    $versions{$fn} = $1;
+	    $maxversion = $1 if $1 > $maxversion;
+	}
+	s/#.*//;
+	if (/^(\S+);D8.*/) {
+	    # do nothing, invalid Unicode
+	} elsif (/^(\S+);([0-9A-Fa-f ]+)/) {
+	    $gmap{$1} = uc($2);
+	}
+    }
+    close O;
+}
+
+doread("glyphlist.txt");
+doread("texglyphlist.txt");
+doread("texglyphlist-g2u.txt");
+
+print "% lcdf-typetools glyphtounicode.tex, Version $maxversion\n";
+print "% Contents: Glyph mapping information for pdftex, used for PDF searching\n";
+print "% Generated from:\n";
+foreach my $a ("glyphlist.txt", "texglyphlist.txt", "texglyphlist-g2u.txt") {
+    print "% - $a, Version ", $versions{$a}, "\n";
+}
+foreach my $a (sort {$a cmp $b} keys %gmap) {
+    print "\\pdfglyphtounicode{$a}{", $gmap{$a}, "}\n";
+}
+
