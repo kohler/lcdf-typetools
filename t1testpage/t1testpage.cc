38d17852a3807a10d2b991191d19263e78c48b14
Eddie Kohler <ekohler@gmail.com>
2011-01-27 09:41:56 -0800

    Correct double-free error.
    Reported by Kurt Pfeifle.

--- t1testpage/t1testpage.cc
+++ t1testpage/t1testpage.cc
@@ -1,6 +1,6 @@
 /* t1testpage.cc -- driver for generating Type 1 fonts' test pages
  *
- * Copyright (c) 1999-2010 Eddie Kohler
+ * Copyright (c) 1999-2011 Eddie Kohler
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -119,7 +119,6 @@ class Testpager { public:
     virtual void font(Type1Font *font, const Vector<PermString>& glyph_names) = 0;
     virtual void epilog() {
 	fprintf(_f, "%%%%EOF\n");
-	fclose(_f);
     }
   protected:
     FILE *_f;
